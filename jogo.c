1e216f64ada778d3c7fecf5ea1d53caf0e17d597
Yurisa-thedestroyer <55629609+Yurisa-thedestroyer@users.noreply.github.com>
2019-11-12 20:19:12 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -1,15 +1,15 @@
 int main(){
-    int const WIDTH = 100;
-    int const HEIGHT = 100;
-    int const CYCLES = 1000;
-    int grid[HEIGHT][WIDTH];
-    int temp[HEIGHT][WIDTH];
+    int const larg = 100; //largura
+    int const alt = 100; //altura
+    int const cicl = 1000; //ciclo
+    int grid[alt][larg];
+    int temp[alt][larg];
 
-    int row;
-    int col;
-    for(row = 0; row < HEIGHT; row++){
-        for(col = 0; col < WIDTH; col++){
-            grid[row][col] = 0;
+    int linha; //linha
+    int col; //coluna
+    for(linha = 0; linha < alt; linha++){
+        for(col = 0; col < larg; col++){
+            grid[linha][col] = 0;
         }
     }
 
@@ -17,41 +17,41 @@ int main(){
     int b;
     int c;
     int neighbours;
-    for(a = 0; a < CYCLES; a++){
-        for(row = 0; row < HEIGHT; row++){
-            for(col = 0; col < WIDTH; col++){
-                temp[row][col] = 0;
+    for(a = 0; a < cicl; a++){
+        for(linha = 0; linha < alt; linha++){
+            for(col = 0; col < larg; col++){
+                temp[linha][col] = 0;
             }
         }
-        for(row = 0; row < HEIGHT; row++){
-            for(col = 0; col < WIDTH; col++){
+        for(linha = 0; linha < alt; linha++){
+            for(col = 0; col < larg; col++){
                 neighbours = 0;
                 for(c = -1; c < 2; c++){
                     for(b = -1; b < 2; b++){
-                        if(b != 0 && c != 0 && grid[(row + c) % HEIGHT][(col + b) % WIDTH] == 1){
+                        if(b != 0 && c != 0 && grid[(linha + c) % alt][(col + b) % larg] == 1){
                             neighbours++;
                         }
                     }
                 }
-                if(grid[row][col] == 1){
+                if(grid[linha][col] == 1){
                     if(neighbours < 2 || neighbours > 3){
-                        temp[row][col] = 0;
+                        temp[linha][col] = 0;
                     }else{
-                        temp[row][col] = 1;
+                        temp[linha][col] = 1;
                     }
-                    else if(grid[row][col] == 0){
+                    else if(grid[linha][col] == 0){
                           if(neighbours == 3){
-                            temp[row][col] = 0;
+                            temp[linha][col] = 0;
                          }else{
-                              temp[row][col] = 1;
+                              temp[linha][col] = 1;
                            }
                       }        
             }
         }
-        for(row = 0; row < HEIGHT; row++){
-            for(col = 0; col < WIDTH; col++){
-                grid[row][col] = temp[row][col];
-                printf("%d", grid[row][col]);
+        for(linha = 0; linha < alt; linha++){
+            for(col = 0; col < larg; col++){
+                grid[linha][col] = temp[linha][col];
+                printf("%d", grid[linha][col]);
             }
             printf("\n");
         }
